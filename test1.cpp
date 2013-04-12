07aefbffce7a0e3edc8a161c7328b59ecae90393
Satori Komeiji <diablomobile.modest@gmail.com>
2013-04-12 22:57:54 +0400

    added testcase

--- test1.cpp
+++ test1.cpp
@@ -2,14 +2,33 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include "list.h"
-BOOST_AUTO_TEST_CASE(mytest_case) {
+#include <iostream>
+BOOST_AUTO_TEST_CASE(test_push) {
 
     list l;
+    
     l.push_back(3);
     BOOST_CHECK_EQUAL(l.back(), 3);
     l.push_back(10);
     BOOST_CHECK_EQUAL(l.back(), 10);
     BOOST_CHECK_EQUAL(l.front(), 3);
+}
+
+BOOST_AUTO_TEST_CASE(test_iterators) {
+    list l;
+    int n = 20;
+    for(int i = 0; i < n; i++) {
+        l.push_back(i);
+    }
+    for(iterator i = l.begin(); i != l.end(); i++) {
+        std::cout << *i << ' ';
+    }
+    std::cout << *l.end() << std::endl;
+    
+    for(int i = 0; i < 20; i++) {
+        std::cout << l.back() << ' ';
+        l.pop_back();
+    }
 }
 
         
